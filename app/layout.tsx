8046a615a827c9047b5dbac7edf24c780527a1c8
cescofors <cescofors75@gmail.com>
2023-11-03 08:54:23 +0100

    geist/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+//import { Inter } from 'next/font/google'
+import { GeistSans, GeistMono } from 'geist/font'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+//const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Francesc Fors Ferrer',
@@ -17,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={GeistMono.className}>
+      <body>{children}</body>
     </html>
   )
 }
