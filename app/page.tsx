12db49d87d607f7a22624cfb766fe8a3494eb1c4
cescofors <cescofors75@gmail.com>
2024-08-16 22:12:38 +0200

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-
 import {Component} from '../components/component/component'
+
 export default function Home() {
   return (
-    
-   
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-start p-4 sm:p-8 md:p-16 lg:p-24">
+      <div className="w-full max-w-5xl">
         <Component />
       </div>
     </main>
-    
   )
 }
